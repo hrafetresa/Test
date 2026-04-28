690d79f70a732da6cf2c1f4342947a5cb15145a8
hrafetresa <hrafetresa@gmail.com>
2026-04-28 10:14:59 +0300

    Добавление файла Book.cs и нового класса Book

---
+++ Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба_1
+{
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public Book(string author, string title, string year)
+        {
+            Author = author;
+            Title = title;
+            Year = year;
+        }
+    }
+}
